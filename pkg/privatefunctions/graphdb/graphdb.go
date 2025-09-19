d1d8e93b2d683c46be5013d359f4bec391cea150
Kaumudi Patil <121973884+kaumudip33@users.noreply.github.com>
2025-09-19 16:16:30 -0700

    Graph DB context and Code validation loop  (#239)

--- pkg/privatefunctions/graphdb/graphdb.go
+++ pkg/privatefunctions/graphdb/graphdb.go
@@ -771,6 +771,55 @@ func (graphdb_context *graphDbContext) WriteCypherQuery(query string, parameters
 
 ////////////// Read functions //////////////
 
+
+// GetNameForKeyValue Graph db query
+func (graphdb_context *graphDbContext) GetNameForKeyValue(searchKey string, searchValue string) (apiSignature string, funcError error) {
+	defer func() {
+		r := recover()
+		if r != nil {
+			logging.Log.Errorf(&logging.ContextMap{}, "Panic GetNameForValue: %v", r)
+			funcError = r.(error)
+			return
+		}
+	}()
+
+	type apiSign struct {
+		Name string `json:"a.Name"`
+	}
+
+
+	// Get pyaedtGroup
+	var query string
+	// Assuming it will always be a method type
+	logging.Log.Debugf(&logging.ContextMap{}, "kapatil: API : %s", searchValue)
+	if searchKey == "name" {
+		query = fmt.Sprintf("MATCH (a:%v) WHERE starts_with(a.name, '%v') RETURN a.Name", "Element", searchValue)
+	} else {
+		query = fmt.Sprintf("MATCH (a:%v) WHERE a.name_pseudocode='%v' RETURN a.Name", "Element", searchValue)
+	}
+	apisOut, err := aali_graphdb.CypherQueryReadGeneric[apiSign](
+		graphdb_context.client,
+		graphdb_context.dbname,
+		query,
+		aali_graphdb.ParameterMap{
+			//"name": aali_graphdb.StringValue(searchValue),
+		},
+	)
+	if err != nil {
+		logging.Log.Errorf(&logging.ContextMap{}, "Error during cypher query: %v", err)
+		return "", err
+	}
+	logging.Log.Debugf(&logging.ContextMap{}, "kapatil: Results apis= %d, Query:  %s", len(apisOut), query)
+	if len(apisOut) > 0 {
+		apiSignature = apisOut[0].Name
+		logging.Log.Debugf(&logging.ContextMap{}, "kapatil: signatures = %s", apiSignature)
+	}
+	return apiSignature, nil
+}
+
+
+
+
 // GetPyaedtGroupCaller gets PyaedtGroup's Caller category element name.
 // Pyaedt Group attribute is a Class specific atttribute, for methods we need to get to
 // its class to find pyaedt group.
@@ -792,6 +841,10 @@ func (graphdb_context *graphDbContext) GetPyaedtGroupCaller(pyaedtGroupInName st
 		}
 	}()
 
+	// Pyaedt Application types /solvers will be called using their own objects
+	if pyaedtGroupInName == "Pyaedt_Application" {
+		return pyaedtGroupInName, nil
+	}
 	type elePyaedtGroup struct {
 		Name string `json:"b.Name"`
 	}
@@ -845,9 +898,39 @@ func (graphdb_context *graphDbContext) GetPyaedtGroupFromCodeGenerationElement(e
 	type elePyaedtGroup struct {
 		Name string `json:"b.Name"`
 	}
-
 	// Get pyaedtGroup
-	// Assuming it will always be a method type
+	// PyaedtGroup is an attribute of class
+	// if entry point is a method, traverse to its class node
+	// TODO: kapatil: check if "parameter" element type need also need to traverse to class
+	if elementType == "Method" {
+		var eleClassNameOut []elePyaedtGroup
+		logging.Log.Debugf(&logging.ContextMap{}, "kapatil:GetPyaedtGroup of element: %s: %s", elementName, elementType)
+		// TODO : kapatil: optimize: to traverse from BelongsTo->IsAPyaedtGroup in single query ?
+		query := fmt.Sprintf("MATCH (a:%v {Name: $name})-[:BelongsTo]->(b:Element) RETURN b.Name", "Element")
+		eleClassNameOut, err := aali_graphdb.CypherQueryReadGeneric[elePyaedtGroup](
+			graphdb_context.client,
+			graphdb_context.dbname,
+			query,
+			aali_graphdb.ParameterMap{
+				"name": aali_graphdb.StringValue(elementName),
+			},
+		)
+		if err != nil {
+			logging.Log.Errorf(&logging.ContextMap{}, "Error during cypher query: %v", err)
+			return "", err
+		}
+		if len(eleClassNameOut) > 0{
+			logging.Log.Debugf(&logging.ContextMap{}, "kapatil: Results class= %s of element = %s, Query:  %s", eleClassNameOut[0].Name, elementName, query)		
+			elementName = eleClassNameOut[0].Name
+		}
+		
+	} else {
+		if elementType != "Class" {
+			return "Pyaedt_Application", nil // default to application type for now
+		}
+	}	
+
+	//Assuming for elementName is a type of "Class"
 	logging.Log.Debugf(&logging.ContextMap{}, "kapatil:GetPyaedtGroup of element: %s: %s", elementName, elementType)
 	query := fmt.Sprintf("MATCH (a:%v {Name: $name})-[:IsAPyaedtGroup]->(b:Element) RETURN b.Name", "Element")
 	elePyaedtGroupOut, err := aali_graphdb.CypherQueryReadGeneric[elePyaedtGroup](
